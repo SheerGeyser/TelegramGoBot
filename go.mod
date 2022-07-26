9cec2a2a1a337288d1d560374cd313040d5e337c
hesher <frolov_sd@ekb.sima-land.ru>
2022-07-26 22:07:14 +0500

    Init main structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SheerGeyser/TelegramGoBot
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+//точка входа
+func main() {
+	fmt.Println(getToken())
+}
+
+//func getUpdates() {}
+
+//func respond() {}
